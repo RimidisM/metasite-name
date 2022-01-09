27559311c77014dea86f8523c09b54c60c0d2d1e
RimidisM <52507290+RimidisM@users.noreply.github.com>
2022-01-09 19:30:16 +0200

    build error handling added

---
+++ src/main/java/lt/metasite/task/app/exceptions/RestResponseExceptionHandler.java
@@ -0,0 +1,20 @@
+package lt.metasite.task.app.exceptions;
+
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {FileUploadException.class})
+    protected ResponseEntity<Object> handleFileUploadException(FileUploadException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ErrorResponse.builder().error(ex.getLocalizedMessage()).build(),
+                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
